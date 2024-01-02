a2d383ce1fecfe2ab80c32377a0d29effd442cf3
CNLohr <lohr85@gmail.com>
2024-01-02 06:06:34 -0500

    Add clock.

--- docs/demofunctions.js
+++ docs/demofunctions.js
@@ -69,5 +69,121 @@ function sirpinski( arr )
 	this.sirpx = sirpx;
 	this.sirpy = sirpy;
 }
+function clockTest( arr )
+{
+	if( !this.clockSetup )
+	{
+		this.place = 0|0;
+		this.clockSetup = true;
+	}
 
-demoFunctions = { juliaIIM, sirpinski };
+	const pxouter = 393;
+	const pxperdiv = 20;
+	const pxperhand = 30;
+	const totalpxo = pxouter + (12*pxperdiv) + 3 * pxperhand;
+	var today = new Date();
+	var seconds = today.getSeconds() + today.getMilliseconds()/1000.0;
+	var minutes= today.getMinutes() + seconds / 60.0;
+	var hours = today.getHours() + minutes/12.0;
+
+
+	for( var i = 1|0; i < 254|0; i+= 2)
+	{
+		var j = this.place++;
+
+		// Do this if you want to make it totally random.
+		//j = Math.random() * (totalpxo);
+
+		// Do this to add a tiny amount of randomness.
+		j += Math.random()*.1;
+
+		let x = -100;
+		let y = -100;
+
+		if( j < pxouter )
+		{
+			x = Math.sin( j * .016 );
+			y = Math.cos( j * .016 );
+		}
+		else if( j < pxouter + (12*pxperdiv) )
+		{
+			let d = ((j - pxouter )/pxperdiv);
+			let digit = d | 0;
+			const shape = [
+				[ 0, -1, 0, 1],
+				[ -1, -1, 1, -1,   1, -1, 1, 0,   1, 0, -1, 0,   -1, 0, -1, 1,   -1, 1, 1, 1 ],
+				[ -1, -1, 1, -1,   1, -1, 1, 0,   1, 0, -1, 0,   1, 0, 1, 1,   -1, 1, 1, 1 ],
+				[ -1, -1, -1, 0,   1, -1, 1, 0,   1, 0, -1, 0,   1, 0, 1, 1 ],
+				[ -1, -1, 1, -1,   -1, -1, -1, 0,   1, 0, -1, 0,   1, 0, 1, 1,   -1, 1, 1, 1 ],
+				[ -1, 0, -1, 1,    1, -1, -1, 0,   1, 0, -1, 0,   1, 0, 1, 1,   -1, 1, 1, 1 ],
+				[ -1, -1, 1, -1,    1, -1, 0, 0,   0, 0, 0, 1,],
+				[ -1, -1, 1, -1,   -1, -1, -1, 0,   1, 0, -1, 0,   1, 0, 1, 1,   -1, 1, 1, 1 ,   -1, 0,-1, 1 ,   1, 0, 1,-1 ],
+				[ -1, 0, -1, -1,   -1,  1, 1, 0,   1, 0, -1, 0,   1, 0, 1,-1,   -1,-1, 1,-1 ],
+				[ -2, -1, -2, 1,   0, -1, 2, -1,  2,-1,2,1,      2,1,0,1,    0,1,0,-1  ],
+				[ -2, -1, -2, 1,   1, -1, 1, 1 ],
+				[ -2, -1, -2, 1,   0, -1, 2, -1,   2, -1, 2, 0,   2, 0, 0, 0,   0, 0, 0, 1,   0, 1, 2, 1 ],
+			];
+			if( digit < shape.length )
+			{
+				ts = shape[digit];
+
+				let segs = (ts.length / 4)|0;
+
+				x = Math.sin( (digit +1)/ 6.0 * 3.13159 ) * 0.8;
+				y = -Math.cos( (digit +1)/ 6.0 * 3.13159 ) * 0.8;
+
+				//let tpl = Math.random() * segs;
+				let tpl = ( d % 1) * segs;
+
+				let tseg = ( tpl ) | 0;
+				console.log( tseg*4+1, ts[tseg*4+1], ts  );
+				let along = tpl % 1;
+				x += lerp( ts[tseg*4+0], ts[tseg*4+2], along ) * 0.05;
+				y += lerp( ts[tseg*4+1], ts[tseg*4+3], along ) * 0.09;
+			}
+		}
+		else if( j < totalpxo  )
+		{
+			let handpx = (j - (pxouter + 12*pxperdiv) ) / pxperhand;
+			let whichhand = handpx | 0;
+			let along = handpx % 1;
+			let ex = 1.0;
+			let ey = 1.0;
+			let angle = 0.0;
+			let len = 1.0;
+			switch( whichhand )
+			{
+				case 0: angle = seconds / 30.0 * 3.14159; len = 0.65; break;
+				case 1: angle = minutes / 30.0 * 3.14159; len = 0.6; break;
+				case 2: angle = hours / 12.0 * 3.14159 * 2.0; len = 0.5; break;
+			}
+			ex = Math.cos( angle - 1.5707 );
+			ey = Math.sin( angle - 1.5707 );
+
+			x = lerp( 0, ex, along ) * len;
+			y = lerp( 0, ey, along ) * len;
+		}
+		else
+		{
+			j = this.place = 0;
+			arr[i+1] = 0;
+			arr[i+0] = 0;	
+			continue;
+		}
+
+		arr[i+1] = (x*60+ 64) & 0x7f;
+		arr[i+0] = (y*60 + 64) & 0x7f;	
+	}
+}
+
+function lerp( x, y, a )
+{
+	return x * (1.0-a) + y * a;
+}
+
+function sigrand()
+{
+	return Math.sqrt( -2.0 * Math.log( 1.0 - Math.random() ) ) * Math.cos( 2.0 * Math.PI * Math.random() );
+}
+
+demoFunctions = { juliaIIM, sirpinski, clockTest };
